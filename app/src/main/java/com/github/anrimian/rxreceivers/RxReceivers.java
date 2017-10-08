c62c894cdabe4ce0bf679180bd62f96d8f016450
Владислав <anrimian@gmail.com>
2017-10-08 19:02:33 +0300

    * change names

--- app/src/main/java/com/github/anrimian/rxreceivers/RxReceivers.java
+++ app/src/main/java/com/github/anrimian/rxreceivers/RxReceivers.java
@@ -16,12 +16,12 @@ import io.reactivex.annotations.NonNull;
 
 public class RxReceivers {
 
-    public static Observable<Intent> create(@NonNull final String action, @NonNull final Context ctx) {
+    public static Observable<Intent> from(@NonNull final String action, @NonNull final Context ctx) {
         IntentFilter filter = new IntentFilter(action);
-        return create(filter, ctx);
+        return from(filter, ctx);
     }
 
-    public static Observable<Intent> create(@NonNull final IntentFilter intentFilter, @NonNull final Context ctx) {
+    public static Observable<Intent> from(@NonNull final IntentFilter intentFilter, @NonNull final Context ctx) {
         return Observable.create(new ObservableOnSubscribe<Intent>() {
             Context appContext = ctx.getApplicationContext();
             @Override
